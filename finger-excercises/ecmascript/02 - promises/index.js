3c4118b23df362031efb25908b705382451ea377
Alan Reskin <areskin@ChurryMAC.local>
2020-10-19 20:25:42 -0300

    02 promises tests

--- finger-excercises/ecmascript/02 - promises/index.js
+++ finger-excercises/ecmascript/02 - promises/index.js
@@ -1,9 +1,33 @@
 // Hint: use setInterval, create a new Promise and measure time with Date.now()
 
-export function delay() {
+/*export function delay(t) {
+    let delayedTime = new Promise (function (resolve, reject){
+        resolve(t);
+    })
+    return delayedTime;
+}*/
 
+
+export function delay(time) { 
+    const start=Date.now();
+    let dt = new Promise (function (resolve, reject){
+        if(time<=1000){
+        setInterval(()=>resolve(Date.now()-start), time);
+    }else{
+        reject(Error('This time is too much !'));
+    }
+    })
+    return dt;
 }
 
-export function asyncDelay() {
-
-}
+ export function asyncDelay(time) {
+    const start=Date.now();
+    let dt = new Promise (function (resolve, reject){
+        if(time<=1000){
+        setInterval(()=>resolve(Date.now()-start), time);
+    }else{
+        reject(Error('This time is too much !'));
+    }
+    })
+    return dt;
+ };
