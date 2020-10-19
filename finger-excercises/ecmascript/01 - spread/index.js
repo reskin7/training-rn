06d3547d3be97bfb129bc7b2250a5a9ff64b168f
Alan Reskin <areskin@ChurryMAC.local>
2020-10-19 20:43:15 -0300

    set master branch

--- finger-excercises/ecmascript/01 - spread/index.js
+++ finger-excercises/ecmascript/01 - spread/index.js
@@ -1,30 +1,13 @@
 import { isArray } from './utils';
 
-export function min(...params) {
-  if (params.length>0) {
-    if (isArray(params[0])) {
-      return Math.min(...params[0]);
-    } else {
-      return Math.min(...params);
-    }
-  } else {
-    return undefined;
-    }   
+export function min() {
+
 }
 
-export function copy(params) {
-  if(isArray(params)){
-return [...params];
-  }else{
-return {...params};
-  }
+export function copy() {
+  
 }
 
-export function reverseMerge(a,b) {
-  return [...b,...a];
-}
-
-export function filterAttribs(params) {
-  const {a, b, ...rest} = params;
-  return rest;
+export function reverseMerge() {
+  
 }
--- finger-excercises/ecmascript/02 - promises/index.js
+++ finger-excercises/ecmascript/02 - promises/index.js
@@ -1,33 +1,9 @@
 // Hint: use setInterval, create a new Promise and measure time with Date.now()
 
-/*export function delay(t) {
-    let delayedTime = new Promise (function (resolve, reject){
-        resolve(t);
-    })
-    return delayedTime;
-}*/
-
-
 export function delay(time) { 
-    const start=Date.now();
-    let dt = new Promise (function (resolve, reject){
-        if(time<=1000){
-        setInterval(()=>resolve(Date.now()-start), time);
-    }else{
-        reject(Error('This time is too much !'));
-    }
-    })
-    return dt;
+
 }
 
  export function asyncDelay(time) {
-    const start=Date.now();
-    let dt = new Promise (function (resolve, reject){
-        if(time<=1000){
-        setInterval(()=>resolve(Date.now()-start), time);
-    }else{
-        reject(Error('This time is too much !'));
-    }
-    })
-    return dt;
+
  };
