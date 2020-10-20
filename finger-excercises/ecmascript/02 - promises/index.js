5d8b80766b63776e8564d623c93f3830384f7632
Alan Reskin <areskin@ChurryMAC.local>
2020-10-19 21:03:37 -0300

    added promises excercises

--- finger-excercises/ecmascript/02 - promises/index.js
+++ finger-excercises/ecmascript/02 - promises/index.js
@@ -1,9 +1,22 @@
 // Hint: use setInterval, create a new Promise and measure time with Date.now()
 
-export function delay(time) { 
+import { delay, asyncDelay } from '.';
 
-}
+const TIME = 500;
 
- export function asyncDelay(time) {
+describe('Promises', () => {
+  it('delay works well', () => delay(TIME)
+    .then(delayedTime => {
+      expect(delayedTime).toBeGreaterThanOrEqual(TIME - 100);
+      expect(delayedTime).toBeLessThanOrEqual(TIME + 100);
+    }));
 
- };
+  it('delay will not wait if the time is too long', () => delay(TIME * TIME)
+    .catch(e => expect(e).toEqual(Error('This time is too much !'))));
+
+  it('asyncDelay also works well', async () => {
+    const delayedTime = await asyncDelay(TIME);
+    expect(delayedTime).toBeGreaterThanOrEqual(TIME - 100);
+    expect(delayedTime).toBeLessThanOrEqual(TIME + 100);
+  });
+});
