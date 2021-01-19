e1c863c3e63e49d4c85ff31b3e47f19a7c2045cf
Alan Reskin <alanreskin@Alans-MacBook-Pro.local>
2021-01-19 17:23:34 -0500

    change training wbooks v2 to v3

--- wbooks/babel.config.js
+++ wbooks/babel.config.js
@@ -1,20 +1,15 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
+    'import-glob',
     [
       'module-resolver',
       {
-        root: ['./src'],
+        root: ['.'],
+        extensions: ['.ios.js', '.android.js', '.js', '.jsx', '.ts', '.tsx', '.json'],
         alias: {
           '@app': './src/app',
-          '@components': '.src/app/components/',
-          '@assets': './src/assets',
-          '@config': './src/config',
-          '@constants': './src/constants',
-          '@interfaces': './src/interfaces',
-          '@redux': './src/redux',
-          '@services': './src/services',
-          '@utils': './src/utils'
+          '@constants': './src/constants'
         }
       }
     ]
